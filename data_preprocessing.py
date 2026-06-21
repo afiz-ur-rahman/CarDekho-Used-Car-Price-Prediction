3fb895aa8f6b0e3930075c58e4585572f3f30d90
AFIZ UR RAHMAN <urrahmanafiz@gmail.com>
2026-06-21 12:17:54 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -58,50 +58,50 @@ jaipur["City"] = "Jaipur"
 df = pd.concat([chennai, delhi, kolkata, bangalore, hyderabad, jaipur], ignore_index=True)
 print("Combined Shape:", df.shape)
 
-print("\nColumn names:") 
+print("\nColumn names:")
 print(df.columns) 
 print(df.head())
-print("\nDataset info:") 
+print("\nDataset info:")
 print(df.info())
 
 # SAVE TO CSV
 df.to_csv(output_file, index=False)
 print("Saved cleaned file as:", output_file)
 
-#Result:
-#PS C:\Users\USER\OneDrive\Desktop\car dekho> python data_preprocessing.py
-#Reading files...
-#Combined Shape: (8369, 6)
-
-#Column names:
-#Index(['new_car_detail', 'new_car_overview', 'new_car_feature',
-#       'new_car_specs', 'car_links', 'City'],
-#      dtype='object')
- #                                     new_car_detail  ...     City
-0 # {'it': 0, 'ft': 'Petrol', 'bt': 'SUV', 'km': '...  ...  Chennai
-1 # {'it': 0, 'ft': 'Petrol', 'bt': 'Minivans', 'k...  ...  Chennai
-2 # {'it': 0, 'ft': 'Petrol', 'bt': 'SUV', 'km': '...  ...  Chennai
-3 # {'it': 0, 'ft': 'Petrol', 'bt': 'Hatchback', '...  ...  Chennai
-4 # {'it': 0, 'ft': 'Petrol', 'bt': 'Hatchback', '...  ...  Chennai
-
-#[5 rows x 6 columns]
-
-#Dataset info:
-#<class 'pandas.core.frame.DataFrame'>
-#RangeIndex: 8369 entries, 0 to 8368
-#Data columns (total 6 columns):
- #   Column            Non-Null Count  Dtype
-#---  ------            --------------  -----
- #0   new_car_detail    8369 non-null   object
- #1   new_car_overview  8369 non-null   object
- #2   new_car_feature   8369 non-null   object
- #3   new_car_specs     8369 non-null   object
- #4   car_links         8369 non-null   object
- #5   City              8369 non-null   object
-#dtypes: object(6)
-#memory usage: 392.4+ KB
-#None
-#Saved cleaned file as: cleaned_car_data.csv
+# Result:
+# PS C:\Users\USER\OneDrive\Desktop\car dekho> python data_preprocessing.py
+# Reading files...
+# Combined Shape: (8369, 6)
+#
+# Column names:
+# Index(['new_car_detail', 'new_car_overview', 'new_car_feature',
+#        'new_car_specs', 'car_links', 'City'],
+#       dtype='object')
+# new_car_detail  ...     City
+# 0  {'it': 0, 'ft': 'Petrol', 'bt': 'SUV', 'km': '...  ...  Chennai
+# 1  {'it': 0, 'ft': 'Petrol', 'bt': 'Minivans', 'k...  ...  Chennai
+# 2  {'it': 0, 'ft': 'Petrol', 'bt': 'SUV', 'km': '...  ...  Chennai
+# 3  {'it': 0, 'ft': 'Petrol', 'bt': 'Hatchback', '...  ...  Chennai
+# 4  {'it': 0, 'ft': 'Petrol', 'bt': 'Hatchback', '...  ...  Chennai
+#
+# [5 rows x 6 columns]
+#
+# Dataset info:
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 8369 entries, 0 to 8368
+# Data columns (total 6 columns):
+#    Column            Non-Null Count  Dtype
+# ---  ------            --------------  -----
+#  0   new_car_detail    8369 non-null   object
+#  1   new_car_overview  8369 non-null   object
+#  2   new_car_feature   8369 non-null   object
+#  3   new_car_specs     8369 non-null   object
+#  4   car_links         8369 non-null   object
+#  5   City              8369 non-null   object
+# dtypes: object(6)
+# memory usage: 392.4+ KB
+# None
+# Saved cleaned file as: cleaned_car_data.csv
  
 
 
