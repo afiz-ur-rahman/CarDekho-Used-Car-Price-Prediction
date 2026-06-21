84d8ae557701a55f494e9a7c8e20b866fff7d079
AFIZ UR RAHMAN <urrahmanafiz@gmail.com>
2026-06-21 23:09:28 +0530

    Update model_development.py

--- model_development.py
+++ model_development.py
@@ -5,6 +5,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error
 
 # ================================
 # LOAD STRUCTURED DATA
@@ -25,6 +27,11 @@ target = "Price"
 
 X = df[features]
 y = df[target]
+
+# ================================
+# SPLIT DATA
+# ================================
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # ================================
 # COLUMN TYPES
@@ -77,20 +84,52 @@ final_model = Pipeline(steps=[
 # ================================
 # TRAIN MODEL
 # ================================
-final_model.fit(X, y)
+final_model.fit(X_train, y_train)
 print("Model training completed.")
 
-print(final_model.feature_names_in_)
+# ================================
+# MODEL EVALUATION
+# ================================
+y_pred = final_model.predict(X_test)
+
+r2 = r2_score(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+
+print("\n===== MODEL EVALUATION =====")
+print("R² Score :", round(r2, 4))
+print("MAE      :", round(mae, 2))
+
+# ================================
+# FEATURES USED
+# ================================
+print("\nFeatures used:")
+print(X.columns.tolist())
+
+# ================================
+# RETRAIN ON FULL DATASET
+# ================================
+final_model.fit(X, y)
 
 # ================================
 # SAVE MODEL
 # ================================
 joblib.dump(final_model, "car_price_model.pkl")
-print("Model saved as car_price_model.pkl")
+print("\nModel saved as car_price_model.pkl")
 
 #Dataset Loaded: (8369, 15)
 #Model training completed.
+#===== MODEL EVALUATION =====
+#R² Score : 0.9333
+#MAE      : 142613.41
+#Features used:
+#['Brand', 'Model', 'Body_Type', 'Fuel', 'Transmission', 'Owner_No', 'Year', 'KM', 'Seats', 'Engine_CC', 'City']
 #Model saved as car_price_model.pkl
+
+
+
+
+
+
 
 
 
